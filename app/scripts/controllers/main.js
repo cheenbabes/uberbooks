dd3b18d61b1c79741895ce1f5142a87dc043e652
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-09-01 11:10:01 -0600

    The geolocation async service needs to be rewritten as it is tripping up in scores.js and map.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,22 +13,38 @@ angular.module('uberbooksApp')
         //eventually this will be paginated to 10 scores a page
 
         $scope.scores = $firebaseArray(Ref.child('scores').limitToLast(50));
-        
+
 
         //get the data for the stats.
         //Currently hardcoded to 24 hours.
         var currentTime = (new Date).getTime();
-        var before = currentTime - 2700000; //86400000;
+        var before = currentTime - 50400000;
 
-        var ref = new Firebase('https://uberbooks.firebaseio.com/scores');
-
-        var query = ref.orderByChild('timestamp').startAt(before).endAt(currentTime);
+        var query = Ref.child('scores').orderByChild('timestamp').startAt(before).endAt(currentTime);
 
         query.on('value', function (snapshot) {
             console.log(snapshot.val());
+            $scope.timescores = $firebaseArray(query);
+
+
+        $scope.timescores.$loaded().then(function (x) {
+            $scope.totalMoney = x.reduce(function (i, score) {
+                return i + parseInt(score.money);
+            }, 0);
+            $scope.totalBooks = x.reduce(function (i, score) {
+                return i + parseInt(score.books);
+            }, 0);
+
+
+        }).catch(function (error) {
+            console.log("Error: ", error);
         });
-    
-    $scope.timescores = $firebaseArray(query);
+        });
+
+        
+
+
+
 
 
         //        var scoresRef = Ref.child("scores");
--- app/scripts/controllers/scores.js
+++ app/scripts/controllers/scores.js
@@ -9,40 +9,43 @@
 angular.module('uberbooksApp')
     .controller('ScoresCtrl', function ($scope, user, Auth, Ref, $firebaseObject, $firebaseArray, FBURL, geolocation, Flash) {
 
+    $scope.scores = $firebaseArray(Ref.child('scores').limitToLast(50));
+    
+    $scope.user = user;
+        
         //grab location
         geolocation.getLocation().then(function (data) {
             $scope.coords = {
                 lat: data.coords.latitude,
                 lon: data.coords.longitude
             };
-        });
-        $scope.profile = $firebaseObject(Ref.child('users/' + user.uid));
 
-        $scope.user = user;
+            $scope.profile = $firebaseObject(Ref.child('users/' + user.uid));
+            $scope.user = user;
+
+            //submit the score
+            $scope.addScore = function () {
+                $scope.scores.$add({
+                    money: $scope.score.money,
+                    books: $scope.score.books,
+                    timestamp: Firebase.ServerValue.TIMESTAMP,
+                    user: $scope.profile.name,
+                    email: $scope.profile.email ? $scope.profile.email : null,
+                    userid: user.uid,
+                    lat: $scope.coords.lat,
+                    lon: $scope.coords.lon
+                });
+                $scope.score.money = '';
+                $scope.score.books = '';
+                Flash.create('success', 'Thank you for submitting your score!');
+
+            };
+
+        });
 
 
         //get the last 10 scores
         $scope.scores = $firebaseArray(Ref.child('scores').limitToLast(10));
-
-
-        //submit the score
-        $scope.addScore = function () {
-            $scope.scores.$add({
-                money: $scope.score.money,
-                books: $scope.score.books,
-                timestamp: Firebase.ServerValue.TIMESTAMP,
-                user: $scope.profile.name,
-                email: $scope.profile.email ? $scope.profile.email : null,
-                userid: user.uid,
-                lat: $scope.coords.lat,
-                lon: $scope.coords.lon
-            });
-            $scope.score.money = '';
-            $scope.score.books = '';
-            Flash.create('success', 'Thank you for submitting your score!'); 
-
-        };
-
 
         //logout now
         $scope.logout = function () {
