1c64c03e6fa151f0c2b32231177a6f9f7e5649fd
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-09-09 14:48:30 -0600

    working polygon areas in user.js. Next step is creating a method in main.js and adding it to the ranking

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -24,8 +24,34 @@ angular.module('uberbooksApp')
                 if (x[i].timestamp >= oneWeekPrevious && x[i].timestamp <= currentTime) {
                     $scope.oneWeekScores.push(x[i]);
                 }
+            };
 
+            //put all the scores coords in an array
+            $scope.oneWeekScoresCoords = []
+            for (var j = 0; j < $scope.oneWeekScores.length; j++) {
+                $scope.oneWeekScoresCoords.push([$scope.oneWeekScores[j].lat, $scope.oneWeekScores[j].lon]);
             }
+
+            var convexHull = new ConvexHullGrahamScan();
+            $scope.oneWeekScoresCoords.forEach(function (item) {
+                convexHull.addPoint(item[1], item[0]);
+            });
+
+            $scope.hullPoints = convexHull.getHull();
+            $scope.pageHullPoints = $scope.hullPoints.map(function (item) {
+                return [item.y, item.x];
+            })
+
+            $scope.googlePolygonPoints = $scope.hullPoints.map(function (item) {
+                return new google.maps.LatLng(item.y, item.x);
+            })
+
+            var polygon = new google.maps.Polygon({
+                paths: $scope.googlePolygonPoints
+            });
+
+            $scope.polygonArea = google.maps.geometry.spherical.computeArea(polygon.getPath());
+
 
         }).catch(function (error) {
             $scope.error = error
