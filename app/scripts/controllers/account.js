19cf71eec35f7ec14d8054bf9af23281735036bd
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-09-25 15:39:03 -0600

    Revert "changes to rules and some changes to grunt file"
    This reverts commit cd5ce9fd6bfeff1873e796559eaf45b3532503a6.

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -8,6 +8,10 @@
  */
 angular.module('uberbooksApp')
     .controller('AccountCtrl', function ($scope, user, Auth, Ref, $firebaseObject, $timeout, geolocation, Flash) {
+        angular.element(document).ready(function () {
+            //Flash.create('info', 'Welcome to your account');
+        });
+
         $scope.user = user;
         $scope.logout = function () {
             Auth.$unauth();
