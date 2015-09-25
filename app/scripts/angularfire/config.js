01af218bd1b41ce431de26223a90d95f77dab4c9
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-09-25 15:44:56 -0600

    commit correct firebase io for test

--- app/scripts/angularfire/config.js
+++ app/scripts/angularfire/config.js
@@ -1,5 +1,5 @@
 angular.module('firebase.config', [])
-    .constant('FBURL', 'https://<your-firebase-here>.firebaseio.com')
+    .constant('FBURL', 'https://uberbookstest.firebaseio.com')
     .constant('SIMPLE_LOGIN_PROVIDERS', ['password'])
 
 .constant('loginRedirectPath', '/login');
